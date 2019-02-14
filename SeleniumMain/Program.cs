8879ed6e7ddfe8572cc675f3d63d7b5c0bc941b6
Paul Herron <pol.herron@gmail.com>
2019-02-14 16:13:17 +0000

    Selenium Starter V2 code cleanup

--- SeleniumMain/Program.cs
+++ SeleniumMain/Program.cs
@@ -14,13 +14,26 @@ namespace SeleniumMain
         static void Main(string[] args)
         {
 
-            OpenPage();
+            IWebDriver chromeDriver = GetChromeDriver();
+            OpenPage(chromeDriver);
+            System.Threading.Thread.Sleep(5000);
+            CloseBrowser(chromeDriver);
 
         }
 
-        public static void OpenPage()
+        private static void CloseBrowser(IWebDriver driver)
+        {
+            driver.Close();
+        }
+
+        private static IWebDriver GetChromeDriver()
         {
             IWebDriver driver = new ChromeDriver(@"c:\libraries\");
+            return driver;
+        }
+
+        public static void OpenPage(IWebDriver driver)
+        {
             driver.Url = "http://www.google.com";
         }
     }
