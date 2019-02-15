df8d89f604f0496d43a6cdb785c91d59312f348e
Paul Herron <pol.herron@gmail.com>
2019-02-15 17:57:42 +0000

    Find select and click on an image.  Includes explicit wait code

--- SeleniumMain/Program.cs
+++ SeleniumMain/Program.cs
@@ -5,8 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Remote;
 using Selenium;
 
@@ -18,41 +16,53 @@ namespace SeleniumMain
         {
 
             IWebDriver chromeDriver = GetChromeDriver();
-            OpenPage(chromeDriver);
-
-            IWebDriver ieDriver = getIEDriver();
-            OpenPage(ieDriver);
-
-            IWebDriver firefoxDriver = getFirefoxDriver();
-            OpenPage(firefoxDriver);
-
-        }
-
-        private static IWebDriver getFirefoxDriver()
-        {
-            IWebDriver driver = new FirefoxDriver(@"c:\libraries\");
-            return driver;
-        }
-
-        private static IWebDriver getIEDriver()
-        {
-            var options = new InternetExplorerOptions
-            {
-                IgnoreZoomLevel = true
-            };
-            IWebDriver driver = new InternetExplorerDriver(@"c:\libraries\",options);
-            return driver;
+           
+            OpenPage(chromeDriver, "https://www.google.com/");
+            DoSearch(chromeDriver);
         }
 
         private static IWebDriver GetChromeDriver()
         {
             IWebDriver driver = new ChromeDriver(@"c:\libraries\");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             return driver;
         }
 
-        public static void OpenPage(IWebDriver driver)
+        public static void OpenPage(IWebDriver driver, string url)
         {
-            driver.Url = "http://www.google.com";
+            driver.Url = url;
         }
+
+        private static void DoSearch(IWebDriver driver)
+        {
+            //var searchTextBox = driver.FindElement(By.ClassName("gLFyf "));
+
+            //Locate the search text box
+            var searchTextBox = driver.FindElement(By.Name("q"));
+
+            //Send text to the text box
+            searchTextBox.SendKeys("pluralsight");
+
+            //Find nad click the search button
+            IWebElement searchButton = driver.FindElement(By.Name("btnK"));
+            searchButton.Click();
+
+            //Find the images link and clck
+            var imagesLink = driver.FindElement(By.LinkText("Images"));
+            imagesLink.Click();
+            
+
+            //Wait for image to be visible
+            var wait = new OpenQA.Selenium.Support.UI.WebDriverWait(driver, new TimeSpan(0, 0, 10));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible
+                (By.ClassName(string.Format("rg_ic"))));
+
+            /*driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);*/
+
+            //Locate and click the image
+            var image = driver.FindElements(By.ClassName("rg_ic"))[0];
+            image.Click();
+        }
+
     }
 }
